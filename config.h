16daa2d7d0769720bf996fe2637ef0a35bc0d8c8
Andrei Muresan <andrei@muresan.io>
2020-01-14 08:55:29 -0800

    add nord colors

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#e5e9f0", "#2e3440" },
+	[SchemeSel] = { "#eceff4", "#5e81ac" },
+	[SchemeOut] = { "#81a1c1", "#d8dee9" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
